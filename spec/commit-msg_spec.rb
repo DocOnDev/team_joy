7b77357400598f7690bb916ef72c9550b2a87d49
Doc Norton <doc@docondev.com>
2019-04-30 19:28:54 -0400

    Confirm failure if no pattern

--- spec/commit-msg_spec.rb
+++ spec/commit-msg_spec.rb
@@ -2,24 +2,28 @@ require_relative 'spec_utils.rb'
 require 'rspec'
 require './lib/commit-msg'
 
-
 describe 'Commit Message Handler' do
 
   before(:each) do
+    puts @resource_dir
     @checker = CheckCommit.new
+    @file_name = SpecUtils::Resource.file("PassingWith3.txt")
+  end
+
+  it 'should tell me the file name' do
+    output = SpecUtils::Capture.stdout { @checker.check @file_name }
+    expect(output).to include "(#{@file_name})"
+  end
+
+  it 'should fail when file content does NOT contain commit pattern' do
+    file_name = SpecUtils::Resource.file("FailingMissing.txt")
+    status = @checker.check file_name
+    expect(status).to eq(ExitCodes.fail)
   end
 
   it 'should succeed when file content does contain commit pattern' do
-    file_name = "resouces/PassingFile.txt"
-    status = @checker.check file_name
+    status = @checker.check @file_name
     expect(status).to eq(ExitCodes.success)
-  end
-
-
-  it 'should tell me the file name' do
-    file_name = 'FileName.txt'
-    output = SpecUtils::Capture.stdout { @checker.check file_name }
-    expect(output).to include "(#{file_name})"
   end
 
 
