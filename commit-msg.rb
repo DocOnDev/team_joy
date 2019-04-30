90ecc028e0db1a3700170883ff35cb38bd09eafe
Doc Norton <doc@docondev.com>
2019-04-30 14:26:37 -0400

    simplified hook

--- commit-msg.rb
+++ commit-msg.rb
@@ -1,23 +1,7 @@
 #!/usr/bin/env ruby
 
 
-$refname = ARGV[0]
-$oldrev  = ARGV[1]
-$newrev  = ARGV[2]
+$temp_commit = ARGV[0]
 $user    = ENV['USER']
 
-puts "Enforcing Policies... \n(#{$refname})"   #" (#{$oldrev[0,6]}) (#{$newrev[0,6]})"
-$regex = /\[BUG: (\d+)\]/
-
-# enforced custom commit message format
-def check_message_format
-  missed_revs = `git rev-list #{$oldrev}..#{$newrev}`.split("\n")
-  missed_revs.each do |rev|
-    message = `git cat-file commit #{rev} | sed '1,/^$/d'`
-    if !$regex.match(message)
-      puts "[POLICY] Your message is not formatted correctly"
-      exit 1
-    end
-  end
-end
-check_message_format
+puts "Checking Commit Message in (#{$temp_commit})"
