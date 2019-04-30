ee51b992fc0f5e340bf87fb881fa58cdb6352ff1
Doc Norton <doc@docondev.com>
2019-04-30 15:36:45 -0400

    Start implementation of file content check

--- lib/commit-msg.rb
+++ lib/commit-msg.rb
@@ -3,15 +3,24 @@
 file_arg = ARGV[0]
 # current_user = ENV['USER']
 
+class ExitCodes
+  def self.fail
+    1
+  end
+
+  def self.success
+    0
+  end
+end
+
 
 class CheckCommit
   def check(commit_file)
     puts "Checking Commit Message in (#{commit_file})"
+    ExitCodes.success
   end
 end
 
 
 check = CheckCommit.new
 check.check(file_arg)
-
-
--- spec/commit-msg_spec.rb
+++ spec/commit-msg_spec.rb
@@ -5,10 +5,20 @@ require './lib/commit-msg'
 
 describe 'Commit Message Handler' do
 
+  before(:each) do
+    @checker = CheckCommit.new
+  end
+
+  it 'should succeed when file content does contain commit pattern' do
+    file_name = "resouces/PassingFile.txt"
+    status = @checker.check file_name
+    expect(status).to eq(ExitCodes.success)
+  end
+
+
   it 'should tell me the file name' do
-    checker = CheckCommit.new
     file_name = 'FileName.txt'
-    output = SpecUtils::Capture.stdout { checker.check file_name }
+    output = SpecUtils::Capture.stdout { @checker.check file_name }
     expect(output).to include "(#{file_name})"
   end
 
