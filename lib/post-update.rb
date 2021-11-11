4343dfbf37fe1df3581f19352a4fd0653bc97452
Doc Norton <doc@docondev.com>
2021-11-11 12:10:16 -0500

    -2- Introduce Basic Configuration
    Create a YAML file for some config settings

--- lib/post-update.rb
+++ lib/post-update.rb
@@ -1,6 +1,13 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'yaml'
+
+GIT_LOG_COMMAND = 'git log -1 HEAD --format=format:"{\"id\":\"%H\",\"shortID\":\"%h\",\"authorName\":\"%an\",\"committerName\":\"%cn\",\"subject\":\"%s\",\"body\":\"%b\"}"'
+GIT_FILES_COMMAND = 'git diff --name-only HEAD~1'
+UPDATE_QUERY = "{ commits { commitMessage score repoCommitShortId authors { name email } repository { name uri } repoCommitId files { name: location } } } "
+
+@config = YAML.load_file('./joy_config.yml')
 
 def run_cli(command)
   %x[#{command}]
@@ -14,9 +21,6 @@ def multi_line_to_array(input)
   input.split(/\n+|\r+/).reject(&:empty?)
 end
 
-GIT_LOG_COMMAND = 'git log -1 HEAD --format=format:"{\"id\":\"%H\",\"shortID\":\"%h\",\"authorName\":\"%an\",\"committerName\":\"%cn\",\"subject\":\"%s\",\"body\":\"%b\"}"'
-GIT_FILES_COMMAND = 'git diff --name-only HEAD~1'
-
 git_details = run_cli(GIT_LOG_COMMAND)
 encoded_details = encode_returns(git_details)
 details_json = JSON.parse(encoded_details)
@@ -28,22 +32,17 @@ details_json.store("files", git_files_array)
 
 puts details_json
 
+uri = URI.parse(@config['cms']['uri'])
 
-uri = URI.parse("https://api-us-east-1.graphcms.com/v2/ckvjz9zck625301z063hx5rlj/master")
-# bearer_token = "%TOKEN%"
-query = "{ commits { commitMessage score repoCommitShortId authors { name email } repository { name uri } repoCommitId files { name: location } } } "
+@request = Net::HTTP::Post.new(uri)
+@request["Accept"] = "application/json"
+@request["Authorization"] = "Bearer " + @config['cms']['token'] unless @config['cms']['public']
+@request.body = JSON.dump({"query" => UPDATE_QUERY})
 
-request = Net::HTTP::Post.new(uri)
-request["Accept"] = "application/json"
-# request["Authorization"] = "Bearer " + bearer_token
-request.body = JSON.dump({"query" => query})
-
-req_options = {
-  use_ssl: uri.scheme == "https",
-}
+req_options = { use_ssl: uri.scheme == "https", }
 
 response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-  http.request(request)
+  http.request(@request)
 end
 
 puts response.code
