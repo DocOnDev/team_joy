be342be8ea205770f9bcb54a37ca841601e8e60a
Doc Norton <doc@docondev.com>
2021-11-12 12:53:54 -0500

    -2- Get branch and location data for the commit

--- lib/post-update.rb
+++ lib/post-update.rb
@@ -3,9 +3,13 @@ require 'uri'
 require 'json'
 require 'yaml'
 
-GIT_LOG_COMMAND = 'git log -1 HEAD --format=format:"{\"id\":\"%H\",\"shortID\":\"%h\",\"authorName\":\"%an\",\"committerName\":\"%cn\",\"subject\":\"%s\",\"body\":\"%b\"}"'
+GIT_URI_COMMAND = "git config --get remote.origin.url"
+
+GIT_LOG_COMMAND = 'git log -1 HEAD --format=format:"{\"id\":\"%H\",\"shortID\":\"%h\",\"authorName\":\"%an\",\"committerName\":\"%cn\",\"committerEmail\":\"%ce\",\"subject\":\"%s\",\"body\":\"%b\"}"'
 GIT_FILES_COMMAND = 'git diff --name-only HEAD~1'
-QUERY = "{ commits { commitMessage score repoCommitShortId authors { name email } repository { name uri } repoCommitId files { name: location } } } "
+
+GIT_CURRENT_BRANCH_COMMAND = 'git branch --show-current'
+GIT_BRANCH_HASH_COMMAND_STUB = "git rev-parse %s"
 
 @config = YAML.load_file('./joy_config.yml')
 
@@ -21,6 +25,21 @@ def multi_line_to_array(input)
   input.split(/\n+|\r+/).reject(&:empty?)
 end
 
+def format_for_query(hash)
+  result = "{"
+  hash.each do |key, value|
+    result += "#{key}: \"#{value}\", "
+  end
+  result + "}"
+end
+
+git_branch = run_cli(GIT_CURRENT_BRANCH_COMMAND)
+git_branch_hash = run_cli(GIT_BRANCH_HASH_COMMAND_STUB % git_branch)
+
+git_location = run_cli(GIT_URI_COMMAND)
+git_location = git_location.gsub(/.*(\@|\/\/)(.*)(\:|\/)([^:\/]*)\/([^\/\.]*)\.git/, 'https://\2/\4/\5/')
+puts "Git Location #{git_location}"
+
 git_details = run_cli(GIT_LOG_COMMAND)
 encoded_details = encode_returns(git_details)
 details_json = JSON.parse(encoded_details)
@@ -30,7 +49,73 @@ git_files_array = multi_line_to_array(commit_files)
 
 details_json.store("files", git_files_array)
 
-puts details_json
+# {"id"=>"d45d0ba71f7d6aa0d031c691926f5a2060536bd4", "shortID"=>"d45d0ba", "authorName"=>"Doc Norton", "committerName"=>"Doc Norton", "subject"=>"-2- Reconciling lack of commit rating from merge", "body"=>"", "files"=>["lib/commit-msg.py", "lib/commit-msg.py.windows"]}
+
+query_data = format_for_query(details_json)
+puts "Query Data: #{query_data}"
+
+QUERY = "mutation { createCommit ( data: #{query_data} ) }"
+
+# mutation makeCommit ($authorEmail: String!, $commitId: String!) {
+#   upsertAuthor (
+#     where: {email: $authorEmail}
+#   	upsert: {
+#       create: { email: "test@docondev.com", name: "Created Test Author" }
+#       update: { email: "test@docondev.com", name: "Test Author" }
+#     }) {
+#     id
+#   }
+#   publishAuthor(where: {email: "test@docondev.com"} to: PUBLISHED) {
+#     id
+#   }
+#   upsertBranch (
+#     where: {hash: "branch-hash"}
+#   	upsert: {
+#       create: {
+#         hash: "branch-hash",
+#         name: "Fake Branch",
+#         repository: {
+#           connect: { uri: "https://github.com/DocOnDev/team_joy"}
+#         }
+#       }
+#       update: {
+#         hash: "branch-hash",
+#         name: "Fake Branch",
+#         repository: {
+#           connect: { uri: "https://github.com/DocOnDev/team_joy"}
+#         }
+#       }
+#     }
+#   ) {
+#     id
+#   }
+#   publishBranch(where:{hash: "branch-hash"} to: PUBLISHED) {
+#     id
+#   }
+#   createCommit (data: {
+#     commitMessage: "Another Fake commit message"
+#     score: 1
+#     repoCommitId: $commitId
+#     repository: {
+#         connect: { uri: "https://github.com/DocOnDev/team_joy"}
+#       }
+#     authors: {
+#       connect: { email: "test@docondev.com" }
+#     }
+#     branch: {
+#       connect: {hash: "branch-hash"}
+#     }
+#   }) {
+#     id
+#   }
+#     publishCommit(where: {repoCommitId: $commitId} to: PUBLISHED) {
+#     id
+#   }
+# }
+
+puts "QUERY: #{QUERY}"
+
+
 
 uri = URI.parse(@config['cms']['uri'])
 
