3ee78cc68255a447c0eab418f9bc47c9aac7f9cf
Doc Norton <doc@docondev.com>
2021-11-13 17:52:36 -0500

    -4- Break commit query into smaller pieces

--- lib/graph_cms.rb
+++ lib/graph_cms.rb
@@ -5,23 +5,29 @@ class GraphCMS
   end
 
   def query
-    raise "Query requires commit hash" unless @git_commit.commit_hash
+    query_string = 'mutation { ' + create_commit + publishCommit + ' }'
+  end
 
-    query_string = 'mutation {
-      createCommit (data: {
-        repoCommitId: "' + @git_commit.commit_hash + '"
-        commitMessage: "' + @git_commit.subject + '"
-        score: ' + (@git_commit.score || 0).to_s + '
-        repository: {
-          connect: { uri: "' + @git_commit.https_location + '"}
-        }
-        authors: {
-          connect: { email: "' + @git_commit.committer_email + '" }
-        }
+  private
 
-      }) { id }
-        publishCommit(where: {repoCommitId: "' + @git_commit.commit_hash + '"} to: PUBLISHED) { id }
-    }
-    '
+  def create_commit
+    raise "Cannot record a commit with a commit hash" unless @git_commit.commit_hash
+
+    'createCommit (data: {
+      repoCommitId: "' + @git_commit.commit_hash + '"
+      commitMessage: "' + @git_commit.subject + '"
+      score: ' + (@git_commit.score || 0).to_s + '
+      repository: {
+        connect: { uri: "' + @git_commit.https_location + '"}
+      }
+      authors: {
+        connect: { email: "' + @git_commit.committer_email + '" }
+      }
+
+    }) { id }'
+  end
+
+  def publishCommit
+    'publishCommit(where: {repoCommitId: "' + @git_commit.commit_hash + '"} to: PUBLISHED) { id }'
   end
 end
