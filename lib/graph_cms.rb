255c9e5060d3a462b41aa2f0fbad05981088af1e
Doc Norton <doc@docondev.com>
2021-11-13 12:47:06 -0500

    -3- GraphCMS.query requires a GitCommit object

--- lib/graph_cms.rb
+++ lib/graph_cms.rb
@@ -1,2 +1,6 @@
 class GraphCMS
+
+  def query
+    raise "Query requires a GitCommit object"
+  end
 end
--- spec/graph_cms_spec.rb
+++ spec/graph_cms_spec.rb
@@ -1,5 +1,13 @@
+require_relative 'spec_utils.rb'
 require 'rspec'
 require './lib/graph_cms'
 
 describe 'GraphCMS' do
+  context 'Without Commit Object' do
+    it 'should raise an error' do
+      graph_cms = GraphCMS.new()
+      expect {graph_cms.query}.to raise_error(/GitCommit/)
+    end
+  end
+
 end
