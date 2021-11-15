d22bceb53a025bf94aa95d4a14236b53bc9f9b04
Doc Norton <doc@docondev.com>
2021-11-15 08:46:48 -0500

    -4- Move variables to let
    Change variable names to be more expressive

--- spec/commit-msg_spec.rb
+++ spec/commit-msg_spec.rb
@@ -3,34 +3,27 @@ require 'rspec'
 require './lib/commit-msg'
 
 describe 'Commit Message Handler' do
-  let(:dirname) { './lib' }
-
-  before(:each) do
-    puts @resource_dir
-    @checker = CheckCommit.new
-    @file_name = SpecUtils::Resource.file("PassingWith3.txt")
-  end
+  let(:output_dir_name) { './lib' }
+  let(:checker) {CheckCommit.new}
+  let(:commit_file_name) {SpecUtils::Resource.file("PassingWith3.txt")}
 
   it 'should tell me the file name' do
-    output = SpecUtils::Capture.stdout { @checker.check @file_name }
-    expect(output).to include "(#{@file_name})"
+    output = SpecUtils::Capture.stdout { checker.check commit_file_name }
+    expect(output).to include "(#{commit_file_name})"
   end
 
 
   context 'file content does contain commit pattern' do
     it 'should succeed' do
-      status = @checker.check @file_name
+      status = checker.check commit_file_name
       expect(status).to eq(ExitCodes.success)
     end
 
     context 'TJ_SCORES file does not exist' do
-      before(:each) do
-        puts "Directory Name: #{dirname}"
-        File.delete(dirname + 'TJ_SCORES') if File.exist?(dirname + 'TJ_SCORES')
-      end
       it 'should create the TJ_SCORES file' do
-        status = @checker.check @file_name
-        expect(File.exist?("#{dirname}/TJ_SCORES")).to be true
+        File.delete(output_dir_name + 'TJ_SCORES') if File.exist?(output_dir_name + 'TJ_SCORES')
+        status = checker.check commit_file_name
+        expect(File.exist?("#{output_dir_name}/TJ_SCORES")).to be true
       end
     end
 
@@ -38,11 +31,11 @@ describe 'Commit Message Handler' do
 
   context 'file content does NOT contain commit pattern' do
     it 'should fail' do
-      file_name = SpecUtils::Resource.file("FailingMissing.txt")
-      content = File.readlines file_name
+      commit_file_name = SpecUtils::Resource.file("FailingMissing.txt")
+      content = File.readlines commit_file_name
 
       expect do
-        output = SpecUtils::Capture.stdout { @checker.check file_name }
+        output = SpecUtils::Capture.stdout { checker.check commit_file_name }
         expect(output).to include "Commit rejected: Message #{content} does not contain a rating between 0 and 5."
       end.to raise_error(SystemExit)
     end
@@ -50,8 +43,8 @@ describe 'Commit Message Handler' do
 
   context 'file content contains pattern with value over 5' do
     it 'should fail' do
-      file_name = SpecUtils::Resource.file("FailingWith6.txt")
-      expect { @checker.check file_name }.to raise_error(SystemExit)
+      commit_file_name = SpecUtils::Resource.file("FailingWith6.txt")
+      expect { checker.check commit_file_name }.to raise_error(SystemExit)
     end
   end
 
