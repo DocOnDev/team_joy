af44ad53fef15e2ecb75857da79806e8a17a156c
Doc Norton <doc@docondev.com>
2021-11-15 14:39:16 -0500

    Include author query in Graph Query

--- lib/graph_cms.rb
+++ lib/graph_cms.rb
@@ -1,5 +1,6 @@
 working_path = Dir.pwd
 require working_path + '/lib/commit_query'
+require working_path + '/lib/author_query'
 
 class GraphCMS
   def initialize(git_commit)
@@ -8,6 +9,11 @@ class GraphCMS
 
   def query
     commit = CommitQuery.new @git_commit
-    'mutation { ' + commit.create_query + commit.publish_query + ' }'
+    author = AuthorQuery.new @git_commit
+    'mutation {
+      ' + author.upsert_query + '
+      ' + author.publish_query + '
+      ' + commit.create_query + '
+      ' + commit.publish_query + ' }'
   end
 end
--- spec/graph_cms_spec.rb
+++ spec/graph_cms_spec.rb
@@ -6,6 +6,7 @@ mock_commit_hash = "Stubbed Commit Hash " + rand(10..1000).to_s
 mock_commit_message = "Running Specs"
 mock_repo_location = "git@github.com:DocOnDev/team_joy.git"
 mock_committer_email = "test@docondev.com"
+mock_committer_name = "Test User"
 mock_commit_branch_name = "mock-branch"
 
 describe 'GraphCMS' do
@@ -18,6 +19,7 @@ describe 'GraphCMS' do
     allow(git_dbl).to receive(:branch_name).and_return(mock_commit_branch_name)
     allow(git_dbl).to receive(:https_location).and_return(mock_repo_location)
     allow(git_dbl).to receive(:committer_email).and_return(mock_committer_email)
+    allow(git_dbl).to receive(:committer_name).and_return(mock_committer_name)
     @graph_cms = GraphCMS.new(git_dbl)
   end
 
@@ -49,6 +51,14 @@ describe 'GraphCMS' do
 
       it 'should have a publishCommit query' do
         expect(@graph_cms.query).to include "publishCommit"
+      end
+
+      it 'should have an upsertAuthor query' do
+        expect(@graph_cms.query).to include "upsertAuthor"
+      end
+
+      it 'should have a publishAuthor query' do
+        expect(@graph_cms.query).to include "publishAuthor"
       end
 
     end
