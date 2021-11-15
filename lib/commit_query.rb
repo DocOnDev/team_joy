de43b6a586ed59b7bd1f6aba8d875f56649d13c3
Doc Norton <doc@docondev.com>
2021-11-14 20:01:58 -0500

    -5- Move Commit Query into separate class

---
+++ lib/commit_query.rb
@@ -0,0 +1,26 @@
+class CommitQuery
+  def initialize(git_commit)
+    @git_commit = git_commit
+  end
+
+  def create_query
+    raise "Cannot record a commit without a commit hash" unless @git_commit.commit_hash
+
+    'createCommit (data: {
+      repoCommitId: "' + @git_commit.commit_hash + '"
+      commitMessage: "' + @git_commit.subject + '"
+      score: ' + (@git_commit.score || 0).to_s + '
+      repository: {
+        connect: { uri: "' + @git_commit.https_location + '"}
+      }
+      authors: {
+        connect: { email: "' + @git_commit.committer_email + '" }
+      }
+
+    }) { id }'
+  end
+
+  def publish_query
+    'publishCommit(where: {repoCommitId: "' + @git_commit.commit_hash + '"} to: PUBLISHED) { id }'
+  end
+end
--- lib/graph_cms.rb
+++ lib/graph_cms.rb
@@ -1,3 +1,5 @@
+require 'commit_query'
+
 class GraphCMS
 
   def initialize(git_commit)
@@ -5,29 +7,7 @@ class GraphCMS
   end
 
   def query
-    'mutation { ' + create_commit + publishCommit + ' }'
-  end
-
-  private
-
-  def create_commit
-    raise "Cannot record a commit with a commit hash" unless @git_commit.commit_hash
-
-    'createCommit (data: {
-      repoCommitId: "' + @git_commit.commit_hash + '"
-      commitMessage: "' + @git_commit.subject + '"
-      score: ' + (@git_commit.score || 0).to_s + '
-      repository: {
-        connect: { uri: "' + @git_commit.https_location + '"}
-      }
-      authors: {
-        connect: { email: "' + @git_commit.committer_email + '" }
-      }
-
-    }) { id }'
-  end
-
-  def publishCommit
-    'publishCommit(where: {repoCommitId: "' + @git_commit.commit_hash + '"} to: PUBLISHED) { id }'
+    commit = CommitQuery.new @git_commit
+    'mutation { ' + commit.create_query + commit.publish_query + ' }'
   end
 end
