ad4bfd03b3674c1954a8c802e0fd229c2429dc65
Doc Norton <doc@docondev.com>
2021-11-14 20:31:02 -0500

    -0- Minor change to continue testing

--- lib/commit_query.rb
+++ lib/commit_query.rb
@@ -8,7 +8,6 @@ class CommitQuery
 
     puts "Git Branch Hash: #{@git_commit.branch_hash}"
 
-    
     'createCommit (data: {
       repoCommitId: "' + @git_commit.commit_hash + '"
       commitMessage: "' + @git_commit.subject + '"
