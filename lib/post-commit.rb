feb125cbe7b4ee3f5cea96fe001b7e31e86f6ce6
Doc Norton <doc@docondev.com>
2021-11-20 15:29:38 -0500

    Remove old comments

--- lib/post-commit.rb
+++ lib/post-commit.rb
@@ -15,64 +15,6 @@ graph_cms = GraphCMS.new(gitCommit)
 
 @config = YAML.load_file("#{dirname}/joy_config.yml")
 
-# mutation makeCommit ($authorEmail: String!, $commitId: String!) {
-#   upsertAuthor (
-#     where: {email: $authorEmail}
-#   	upsert: {
-#       create: { email: "test@docondev.com", name: "Created Test Author" }
-#       update: { email: "test@docondev.com", name: "Test Author" }
-#     }) {
-#     id
-#   }
-#   publishAuthor(where: {email: "test@docondev.com"} to: PUBLISHED) {
-#     id
-#   }
-#   upsertBranch (
-#     where: {hash: "branch-hash"}
-#   	upsert: {
-#       create: {
-#         hash: "branch-hash",
-#         name: "Fake Branch",
-#         repository: {
-#           connect: { uri: "https://github.com/DocOnDev/team_joy"}
-#         }
-#       }
-#       update: {
-#         hash: "branch-hash",
-#         name: "Fake Branch",
-#         repository: {
-#           connect: { uri: "https://github.com/DocOnDev/team_joy"}
-#         }
-#       }
-#     }
-#   ) {
-#     id
-#   }
-#   publishBranch(where:{hash: "branch-hash"} to: PUBLISHED) {
-#     id
-#   }
-#   createCommit (data: {
-#     commitMessage: "Another Fake commit message"
-#     score: 1
-#     repoCommitId: $commitId
-#     repository: {
-#         connect: { uri: "https://github.com/DocOnDev/team_joy"}
-#       }
-#     authors: {
-#       connect: { email: "test@docondev.com" }
-#     }
-#     branch: {
-#       connect: {hash: "branch-hash"}
-#     }
-#   }) {
-#     id
-#   }
-#     publishCommit(where: {repoCommitId: $commitId} to: PUBLISHED) {
-#     id
-#   }
-# }
-
-
 uri = URI.parse(@config['cms']['uri'])
 
 @request = Net::HTTP::Post.new(uri)
