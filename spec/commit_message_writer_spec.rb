447d018105d8db5cdc24d5cfb3af1cd8f3e5369b
Doc Norton <doc@docondev.com>
2022-01-04 15:32:04 -0500

    Fix error in message writer spec
    Was quietly causing all other specs to not run

--- spec/commit_message_writer_spec.rb
+++ spec/commit_message_writer_spec.rb
@@ -1,8 +1,10 @@
 require_relative 'spec_utils.rb'
 require 'rspec'
 require './lib/commit_message_writer'
+require './lib/commit_message'
 
 COMMIT_MESSAGE_FILE = File.expand_path(File.dirname(__FILE__)) + "/COMMIT_MSG_FILE"
+BODY = "This is the body line one.\nThis is the body line two."
 
 describe 'Commit Message Writer' do
   context 'given no commit message' do
@@ -13,11 +15,11 @@ describe 'Commit Message Writer' do
 
   context 'given a commit message with a subject and body' do
     let(:writer){CommitMessageWriter.new(@message)}
-    SUBJECT = "This is the subject line with a body"
-    BODY = "This is the body line one.\nThis is the body line two."
+
     before(:each) do
       @message = CommitMessage.new
-      @message.subject = SUBJECT
+      @subject = "This is the subject line with a body"
+      @message.subject = @subject
       @message.body = BODY
     end
 
@@ -25,7 +27,7 @@ describe 'Commit Message Writer' do
       writer.write_to_file(COMMIT_MESSAGE_FILE)
 
       content = File.readlines(COMMIT_MESSAGE_FILE)
-      expect(content[0].chomp).to eq(SUBJECT)
+      expect(content[0].chomp).to eq(@subject)
       lines = BODY.split("\n")
       expect(content[1]).to eq("\n")
       expect(content[2].chomp).to eq(lines[0])
@@ -35,17 +37,19 @@ describe 'Commit Message Writer' do
 
   context 'given a commit message with only a subject' do
     let(:writer){CommitMessageWriter.new(@message)}
-    SUBJECT = "This is the solo subject line"
+
     before(:each) do
       @message = CommitMessage.new
-      @message.subject = SUBJECT
+      @subject = "This is the solo subject line"
+      @message.subject = @subject
     end
 
     it 'should write the subject and no body' do
+      puts "Subject and NO Body"
       writer.write_to_file(COMMIT_MESSAGE_FILE)
 
       content = File.readlines(COMMIT_MESSAGE_FILE)
-      expect(content[0].chomp).to eq(SUBJECT)
+      expect(content[0].chomp).to eq(@subject)
       expect(content[1]).to eq("\n")
       expect(content[2]).to be_nil
 
