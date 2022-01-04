08cd5ec13f2c64a197d78b1d6ff67580fcba6621
Doc Norton <doc@docondev.com>
2022-01-04 09:27:54 -0500

    Make GitCommitMessageAdapter static
    Pass it a file, it returns a commit message object

--- lib/git_commit-msg_adapter.rb
+++ lib/git_commit-msg_adapter.rb
@@ -1,6 +1,8 @@
+require 'commit_message'
+
 class GitCommitMessageAdapter
 
-  def message_from_file commit_file_name
+  def self.message_from_file(commit_file_name)
     raise ArgumentError.new("not a valid file") unless File.file?(commit_file_name)
 
     commit_message = CommitMessage.new
@@ -14,27 +16,31 @@ class GitCommitMessageAdapter
   end
 
 private
-  def load_content_from_file file_name
+  def self.load_content_from_file file_name
     File.readlines file_name
   end
 
-  def get_score_from content
-    get_subject_line_from(content).scan(/-(\d)-/).first[0].to_i
+  def self.get_score_from content
+    scoreList = get_subject_line_from(content).scan(/-(\d)-/).first
+    if scoreList
+      return scoreList[0].to_i
+    end
+    return -1
   end
 
-  def get_subject_from content
+  def self.get_subject_from content
     subject_line = get_subject_line_from(content)
     subject_line.slice!("-#{get_score_from(content)}- ")
     subject_line.strip()
   end
 
-  def get_body_from content
+  def self.get_body_from content
     content_without_subject = content.difference([get_subject_line_from(content)])
     content_without_subject.shift if content_without_subject[0] == "\n"
     content_without_subject.join()
   end
 
-  def get_subject_line_from content
+  def self.get_subject_line_from content
     String.new(content[0])
   end
 
--- spec/git_commit-msg_adapter_spec.rb
+++ spec/git_commit-msg_adapter_spec.rb
@@ -7,8 +7,7 @@ describe 'Git Commit Message Adapter' do
     let(:commit_file) {"bad_file_name.txt"}
 
     it "should report an argument error" do
-      adapter = GitCommitMessageAdapter.new
-      expect {adapter.message_from_file(commit_file)}.to raise_error(ArgumentError, /not a valid file/)
+      expect {GitCommitMessageAdapter.message_from_file(commit_file)}.to raise_error(ArgumentError, /not a valid file/)
     end
   end
 
@@ -17,13 +16,12 @@ describe 'Git Commit Message Adapter' do
     let(:commit_file_with_4) {SpecUtils::Resource.file("PassingWith4.txt")}
 
     before(:each) do
-      @adapter = GitCommitMessageAdapter.new
-      @commitMessage = @adapter.message_from_file(commit_file)
+      @commitMessage = GitCommitMessageAdapter.message_from_file(commit_file)
     end
 
     it "should have a valid score" do
       expect(@commitMessage.score).to eq(3)
-      expect(@adapter.message_from_file(commit_file_with_4).score).to eq(4)
+      expect(GitCommitMessageAdapter.message_from_file(commit_file_with_4).score).to eq(4)
     end
 
     it "should have a valid subject" do
@@ -39,8 +37,7 @@ describe 'Git Commit Message Adapter' do
     let(:commit_file) {SpecUtils::Resource.file("PassingWith2.txt")}
 
     before(:each) do
-      @adapter = GitCommitMessageAdapter.new
-      @commitMessage = @adapter.message_from_file(commit_file)
+      @commitMessage = GitCommitMessageAdapter.message_from_file(commit_file)
     end
 
     it "should have a valid score" do
@@ -53,6 +50,13 @@ describe 'Git Commit Message Adapter' do
 
     it "should have an empty message body" do
       expect(@commitMessage.body).to eq("")
+    end
+  end
+
+  context "commit message without score" do
+    let(:commit_file) {SpecUtils::Resource.file("FailingMissing.txt")}
+    it 'should fail' do
+      expect{ GitCommitMessageAdapter.message_from_file(commit_file) }.to raise_error(SystemExit, /between 0 and 5/)
     end
   end
 end
