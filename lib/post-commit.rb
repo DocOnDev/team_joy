c247acebcc06e7059132699616c5b1cc8b6a49d3
Doc Norton <doc@docondev.com>
2022-01-05 11:48:37 -0500

    Move Requestor class into own file

--- lib/post-commit.rb
+++ lib/post-commit.rb
@@ -1,36 +1,8 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
-require 'uri'
-require 'json'
-require 'yaml'
-require_relative 'joy_config'
 require_relative 'git_commit'
 require_relative 'graph_cms_query'
-
-class GraphCmsRequestor
-
-  def self.execute(query)
-    config = JoyConfig.new()
-
-    cms_uri = URI.parse(config.cms_uri)
-
-    request = Net::HTTP::Post.new(cms_uri)
-    request["Accept"] = "application/json"
-    request["Authorization"] = "Bearer " + config.cms_token unless config.is_cms_public?
-    req_options = { use_ssl: cms_uri.scheme == "https", }
-
-    request.body = JSON.dump({"query" => query})
-
-    response = Net::HTTP.start(cms_uri.hostname, cms_uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    return response
-
-  end
-
-end
+require_relative 'graph_cms_requestor'
 
 def formulate_query()
   git_commit = GitCommit.new
