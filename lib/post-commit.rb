24718df9d7265f61efe30ee7c8460b39fbb2b072
Doc Norton <doc@docondev.com>
2022-01-05 11:59:47 -0500

    Remove temporary variable

--- lib/post-commit.rb
+++ lib/post-commit.rb
@@ -4,9 +4,7 @@ require_relative 'graph_cms_query'
 require_relative 'graph_cms_requestor'
 
 graph_query = GraphCmsQuery.new()
-query = graph_query.query
-
-response = GraphCmsRequestor.execute(query)
+response = GraphCmsRequestor.execute(graph_query.query)
 
 puts response.code
 puts response.body
