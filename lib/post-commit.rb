64b2520eb6398167ade4cf253138203286706b60
Doc Norton <doc@docondev.com>
2022-01-10 15:03:24 -0600

    Minor change to confirm hooks still work
    Deployed last change.

--- lib/post-commit.rb
+++ lib/post-commit.rb
@@ -3,11 +3,11 @@
 require_relative 'git_graph_cms_query'
 require_relative 'graph_cms_requestor'
 
-# Pass commit to CommitSaveQuery object
-# Pass CommitSaveQuery.graph_cms_query to GraphCmsRequestor
-
 # Create general commit object - GitCommitAdapter
 commit = GitCommitAdapter.transform_commit
+
+# Pass commit to CommitSaveQuery object
+# Pass CommitSaveQuery.graph_cms_query to GraphCmsRequestor
 
 git_graph_query = GitGraphCmsQuery.new()
 response = GraphCmsRequestor.execute(git_graph_query.query)
