9761c2c38dac301bf640a49ffb79036ab1dda054
Doc Norton <doc@docondev.com>
2022-01-14 16:17:51 -0600

    Create GraphCmsQuery that uses new GraphCms Commit and Author
    Now the GraphCms query knows nothing about Git

---
+++ lib/graph_cms_query.rb
@@ -0,0 +1,15 @@
+require_relative 'graph_cms_commit_query'
+require_relative 'graph_cms_author_query'
+
+class GraphCmsQuery
+
+  def create_commit(commit)
+    commit_query = GraphCmsCommitQuery.new commit
+    author_query = GraphCmsAuthorQuery.new commit.author
+    'mutation {
+      ' + author_query.upsert + '
+      ' + author_query.publish + '
+      ' + commit_query.create + '
+      ' + commit_query.publish + ' }'
+  end
+end
---
+++ spec/graph_cms_query_spec.rb
@@ -0,0 +1,63 @@
+require_relative 'spec_utils.rb'
+require 'rspec'
+require './lib/graph_cms_query'
+
+mock_hash = SpecUtils::MockResponse.commit_hash
+
+
+describe 'GraphCmsQuery' do
+  let(:commit_dbl){double(Commit)}
+  let(:author_dbl){double(Author)}
+
+  before(:each) do
+    allow(commit_dbl).to receive(:id).and_return(mock_hash)
+    allow(commit_dbl).to receive(:score).and_return(3)
+    allow(commit_dbl).to receive(:subject).and_return(SpecUtils::MockResponse.commit_message)
+    allow(commit_dbl).to receive(:body).and_return(SpecUtils::MockResponse.body)
+    allow(commit_dbl).to receive(:branch_name).and_return(SpecUtils::MockResponse.branch_name)
+    allow(commit_dbl).to receive(:https_location).and_return(SpecUtils::MockResponse.repo_location)
+    allow(commit_dbl).to receive(:author).and_return(author_dbl)
+    allow(commit_dbl).to receive(:files).and_return(SpecUtils::MockResponse.files)
+
+    allow(author_dbl).to receive(:email).and_return(SpecUtils::MockResponse.committer_email)
+    allow(author_dbl).to receive(:name).and_return(SpecUtils::MockResponse.committer_name)
+
+    @graph_query = GraphCmsQuery.new
+  end
+
+  describe 'create_commit' do
+    context 'missing a commit id' do
+      it 'should raise an error' do
+        allow(commit_dbl).to receive(:id)
+        expect {@graph_query.create_commit(commit_dbl)}.to raise_error(/commit id/)
+      end
+    end
+
+    context 'missing a score' do
+      it 'should default score to 0' do
+        allow(commit_dbl).to receive(:score).and_return(nil)
+        expect(@graph_query.create_commit(commit_dbl)).to include "score: 0"
+      end
+    end
+
+    context 'with a good commit' do
+      it 'should have a createCommit query' do
+        expect(@graph_query.create_commit(commit_dbl)).to include "createCommit"
+      end
+
+      it 'should have a publishCommit query' do
+        expect(@graph_query.create_commit(commit_dbl)).to include "publishCommit"
+      end
+
+      it 'should have an upsertAuthor query' do
+        expect(@graph_query.create_commit(commit_dbl)).to include "upsertAuthor"
+      end
+
+      it 'should have a publishAuthor query' do
+        expect(@graph_query.create_commit(commit_dbl)).to include "publishAuthor"
+      end
+
+    end
+  end
+
+end
