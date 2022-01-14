dfa88a0d07465b019521fa39a252f381cf45fdfe
Doc Norton <doc@docondev.com>
2022-01-14 15:51:51 -0600

    Create GraphCmsAuthorQuery that works with generic author

---
+++ lib/graph_cms_author_query.rb
@@ -0,0 +1,24 @@
+class GraphCmsAuthorQuery
+  def initialize(author)
+    @author = author
+  end
+
+  def upsert_query
+    raise "Cannot record an author without an author email" unless @author.email
+
+    author_email = @author.email
+    'upsertAuthor (
+      where: {email: "' + author_email + '"}
+      upsert: {
+        create: { email: "' + author_email + '", name: "' + @author.name + '" }
+        update: { email: "' + author_email + '", name: "' + @author.name + '" }
+      })
+    {id}'
+  end
+
+  def publish_query
+    raise "Cannot record an author without an author email" unless @author.email
+
+    'publishAuthor(where: {email: "' + @author.email + '"} to: PUBLISHED) {id}'
+  end
+end
