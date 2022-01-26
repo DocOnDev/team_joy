abaf8be2231a7785138a372151b2298c391b1334
Doc Norton <doc@docondev.com>
2022-01-26 16:54:21 -0500

    Commit Subject must be a string

--- lib/commit.rb
+++ lib/commit.rb
@@ -1,10 +1,15 @@
 class Commit
-  attr_accessor :subject, :body, :score, :branch_name, :files, :uri, :author
-  attr_reader  :id
+  attr_accessor :body, :score, :branch_name, :files, :uri, :author
+  attr_reader  :id, :subject
 
   def id=(id)
     abort "A commit id must be a String" unless id.is_a?(String)
     @id = id
+  end
+
+  def subject=(subject)
+    abort "A subject must be a String" unless subject.is_a?(String)
+    @subject = subject
   end
 
 
--- spec/commit_spec.rb
+++ spec/commit_spec.rb
@@ -18,4 +18,20 @@ describe 'Commit' do
       expect(commit.id).to eq("String ID")
     end
   end
+
+  context "Subject Property" do
+    it 'should reject a non-String value' do
+      expect do
+        output = SpecUtils::Capture.stdout { commit.subject = 42 }
+        expect(output).to include "A commit subject must be a String"
+      end.to raise_error(SystemExit)
+    end
+
+    it 'should accept a String value' do
+      commit.subject = "String Subject"
+      expect(commit.subject).to eq("String Subject")
+    end
+  end
+
+
 end
