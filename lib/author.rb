c82e1b9c5fe3238b1b2781939758bf56c81e34c5
Maruan Aguerdouh <maruan.mx@gmail.com>
2022-01-28 12:43:42 +0100

    Added: Email and Name validations

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,22 @@
 class Author
   attr_accessor :name, :email
 
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   def initialize(name, email)
-    @name = name
-    @email = email
+    validations(name, email)
+    @name = name.to_s
+    @email = email.to_s
   end
 
   def ==(other)
     self.name  == other.name &&
     self.email == other.email
+  end
+
+  private
+
+  def validations(name, email)
+    raise StandardError, 'Email must have proper format.' if (email =~ EMAIL_FORMAT).nil?
   end
 end
